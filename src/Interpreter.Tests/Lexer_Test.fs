88df9a9adc3cccebc8b4e5eb665b3a0d34c0b049
Sven Grosen <svengrosen@gmail.com>
2017-11-04 15:08:26 -0500

    Lexer defined

--- src/Interpreter.Tests/Lexer_Test.fs
+++ src/Interpreter.Tests/Lexer_Test.fs
@@ -1,7 +1,31 @@
-﻿module Lexer_Test
+﻿namespace Tests
 
 open NUnit.Framework
-open Interpreter
+open Monkey.Token
+open Monkey.TokenConsts
+open Monkey.Lexer
 
+module LexerTests =
 
+    let nextTokenTestCases =
+        [
+            ASSIGN, "="
+            PLUS, "+"
+            LPAREN, "("
+            RPAREN, ")"
+            LBRACE, "{"
+            RBRACE, "}"
+            COMMA, ","
+            SEMICOLON, ";"
+            EOF, ""
+        ] |> List.map(fun (t, l) -> TestCaseData(t, l))
+    
+    [<TestCaseSource("nextTokenTestCases")>]
+    let testNextToken(expectedTokenType:TokenType, expectedLiteral:string) =
+        let input = "=+(){},;"
+
+        let lexer = createLexer input
+        let token = nextToken lexer
+        Assert.AreEqual(expectedTokenType, token.Type)
+        Assert.AreEqual(expectedLiteral, token.Literal)
 

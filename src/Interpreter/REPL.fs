f636d08e252397d34e48b472d9cc49d9058c0138
Sven Grosen <svengrosen@gmail.com>
2017-12-02 15:44:27 -0600

    Rename projects/solution to Monkey and add untested REPL

---
+++ src/Interpreter/REPL.fs
@@ -0,0 +1,20 @@
+﻿namespace Monkey
+
+open System.IO
+open Token
+open Lexer2
+
+module Repl =
+
+    [<Literal>]
+    let PROMPT = ">> "
+
+    let start (reader:TextReader) (writer:TextWriter) =
+        let rec startRec =
+            // todo: async!
+            writer.Write(PROMPT) |> ignore
+            let scanned = reader.ReadLine()
+            let tokens = tokenizeInput scanned
+            for i in 0..tokens.Length-1 do
+                writer.WriteLine(tokens.[i])
+        startRec
---
+++ src/MonkeyRepl/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Monkey.Repl
+
+[<EntryPoint>]
+let main argv = 
+    
+    printfn "Hello! Welcome to the Monkey Programming Language!"
+    printfn "Feel free to type in commands\n"
+    
+    start stdin stdout
+
+    0 // return an integer exit code
